0888fbea5702e243214aadb2aa6a0ff7692c458e
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-07 11:07:14 +0000

    validates

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,23 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   
-  belongs_to :user
+  belongs_to :user, optional: true
+  belongs_to :tag, optional: true
   
-  has_one_attached :image
+  has_many_attached :images
   
-  # バリデーション
-  with_options presence: true do
-    validates :title
-    validates :content
-  end
+  validates :title, presence: true, length: { minimum: 1,maximum: 30 }
+  validates :content, presence: true, length: { minimum: 1,maximum: 500 }
   
   def bookmarked_by(user)
     bookmarks.exists?(user_id: user.id)
+  end
+  
+  def get_image(width, height)
+    image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  def self.looks(word)
+    Post.where("title LIKE? OR content LIKE?", "%#{word}%","%#{word}%")
   end
 end

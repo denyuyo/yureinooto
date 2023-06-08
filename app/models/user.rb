e0382607530ff78e415c81387eeae50eb007018a
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-08 13:18:27 +0000

    Signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,27 +1,22 @@
 class User < ApplicationRecord
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
-  
-  # バリデーション
-  with_options presence: true do
-    validates :name
-    validates :email, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
-    validates :password
-  end
-  
+
+  validates :name, presence: true
+
   has_one_attached :profile_image
-  
+
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.png'
   end
-  
+
   def bookmarked_by(post)
     bookmarks.exists?(post_id: post.id)
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,24 +17,16 @@ devise_for :admin, skip: [:registrations, :passwords], controllers: {
       resources :posts, only: [:index, :show, :destroy]
     end
 
-# scope module: :public do
- 
-  root to: "public/homes#top"
-  get "/about" => "public/homes#about"
-  
-  resources :posts, only: [:index, :create, :show, :edit, :destroy]
-   resource :bookmarks,  only: [:index, :create, :show, :destroy]
-   
-   
-   # ユーザープロフィール関連
-  get '/users/profile/:id', to: 'public/users#show', as: 'user_profile'
-  get '/users/profile/:id/edit', to: 'public/users#edit', as: 'edit_user_profile'
-  patch '/users/profile', to: 'public/users#update', as: 'update_user_profile'
+  namespace :public do
+    root to: "homes#top"
+    get "/about" => "homes#about"
 
-  # ユーザー一覧と詳細
-  get '/users', to: 'public/users#index', as: 'users'
-  get '/users/:id', to: 'public/users#show', as: 'user'
-  
-# end
+    resources :posts, only: [:index, :show, :edit, :create, :destroy, :update] do
+      resource :bookmarks, only: [:create, :destroy]
+      resources :comments, only: [:create, :destroy]
+    end
+
+    resources :users, only: [:index, :show, :edit, :update]
+  end
 
 end

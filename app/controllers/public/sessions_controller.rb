4e20d3ff95eaab36f0093787a2915fa2dbb19f9f
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-10 09:40:44 +0000

    logincompleate

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -1,2 +1,16 @@
 class Public::SessionsController < Devise::SessionsController
+  def create
+    # パラメータからユーザー名とパスワードを取得
+    username = params[:user][:name]
+    password = params[:user][:password]
+
+    # ユーザーの認証を行う
+    user = User.find_by(name: username)
+    if user && user.valid_password?(password)
+      sign_in(user) # ユーザーをログイン状態にする
+      redirect_to root_path, notice: "ログインしました" # ログイン成功時のリダイレクト先を設定
+    else
+      redirect_to new_user_session_path, alert: "ログインに失敗しました" # ログイン失敗時のリダイレクト先を設定
+    end
+  end
 end

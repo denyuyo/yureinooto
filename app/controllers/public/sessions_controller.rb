6f059375e33ab9cceb4b7211b1916c7e6a12abfa
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-11 05:38:57 +0000

    toplayout

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -13,4 +13,9 @@ class Public::SessionsController < Devise::SessionsController
       redirect_to new_user_session_path, alert: "ログインに失敗しました" # ログイン失敗時のリダイレクト先を設定
     end
   end
+  
+  def destroy
+    sign_out(current_user) # ユーザーをログアウト状態にする
+    redirect_to root_path, notice: "ログアウトしました" # ログアウト成功時のリダイレクト先を設定
+  end
 end

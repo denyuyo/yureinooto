2f287fe8240ac7fc84d3979d7ebb0cbdfefb8d12
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-12 04:30:28 +0000

    comment save[done]

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,39 +1,29 @@
 class Public::CommentsController < ApplicationController
-  
-  before_action :set_post, only: [:new, :create]
-  before_action :set_comment, only: [:destroy]
-
-  def new
-    @comment = Comment.new
-  end
-
-  def create
-    @comment = @post.comments.build(comment_params)
-    if @comment.save
-      redirect_to post_path(@post), notice: 'コメントが投稿されました。'
-    else
-      redirect_to post_path(@post), alert: 'コメントの投稿に失敗しました。'
-    end
-  end
 
   def index
-    @comments = Comment.all
+    @user = User.find(params[:id])
+    @comments = Comment.where(user_id: @user.id)
   end
-
+  
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.new(comment_params)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to post_path(@post), notice: "コメントを送信しました"
+    else
+      flash.now[:alert] = "コメントの送信に失敗しました"
+      render 'public/posts/show'
+    end
+  end
+  
   def destroy
+    @comment = Comment.find(params[:id])
     @comment.destroy
-    redirect_to comments_path, notice: 'コメントが削除されました。'
+    redirect_to request.referer
   end
 
   private
-
-  def set_post
-    @post = Post.find(params[:post_id])
-  end
-
-  def set_comment
-    @comment = Comment.find(params[:id])
-  end
 
   def comment_params
     params.require(:comment).permit(:content)

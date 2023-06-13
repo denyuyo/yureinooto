d209748dcce129294ca19781ff2888e0d1a96ab6
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-13 18:17:58 +0000

    [create]tag_all

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :posts
+  has_and_belongs_to_many :posts
   
   validates :tag_name, presence: true, uniqueness: true, length: { minimum: 1,maximum: 15 }
   

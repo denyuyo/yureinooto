07f4cecefbcf3e0e96e98c53a910eccc75133b5d
denyuyo <129641290+denyuyo@users.noreply.github.com>
2023-06-15 07:34:50 +0000

    [Add]admin_controller

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,2 +1,7 @@
 class Admin::CommentsController < ApplicationController
+  
+  def index
+    @user = User.find(params[:id])
+    @comment = Postcomment.where(user_id: @user.id)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,8 +4,26 @@ class Comment < ApplicationRecord
   belongs_to :post
   
   has_many :notifications, dependent: :destroy
-
   
   validates :content, presence: true, length: { minimum: 1,maximum: 500 }
   
+  def create_notification_comment(visitor, post_id)
+    user_id = Post.find(post_id).user.id
+    save_notification_comment(visitor, post_id, user_id)
+  end
+  
+  def save_notification_comment(visitor, post_id, visited_id)
+    # コメントは複数回することが考えられるため、１つの投稿に複数回通知する
+    notification = visitor.active_notifications.new(
+      post_id: post_id,
+      comment_id: id,
+      visited_id: visited_id,
+      action: 'comment'
+    )
+    # 自分の投稿に対するコメントの場合は、通知済みとする
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
+  end
 end
